08e3c65739bd0d9c8f639eb2203b018733f70774
thalitha <campos.talita@ymail.com>
2019-12-10 22:19:03 +0000

    create index html

--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Ecommerce</h1><h2>welcome</h2>')
+    return render(request,'index.html')
