f5dddbadb78592b89c82d9343331361e8176c686
thalitha <campos.talita@ymail.com>
2019-12-11 22:30:17 +0000

    add new coins

--- ecommerceapp/views.py
+++ ecommerceapp/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
+
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    products ={
+        1:"Awesome Brown",
+        2:"Light Pink and White",
+        3:"Black and Pink"
+    }
+
+    dados = {
+        "product_names":products
+    }
+    return render(request,'index.html', dados)
 
 def product(request):
     return render(request,'product.html')
