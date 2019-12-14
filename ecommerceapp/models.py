c690ba66729efc12af483e42190f3bb7f1ef3f5c
thalitha <campos.talita@ymail.com>
2019-12-14 12:30:27 +0000

    creation of the tables order and items

--- ecommerceapp/models.py
+++ ecommerceapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import datetime
 
+
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=200)
@@ -9,3 +10,12 @@ class Product(models.Model):
     created_date = models.DateTimeField(default=datetime.now, blank=True)
     category = models.CharField(max_length=200)
     image = models.ImageField(upload_to="img", blank=True, null=True)
+
+class Order(models.Model):
+    user_id = models.IntegerField()
+    created_date = models.DateTimeField(default=datetime.now, blank=True)
+
+class Item(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=datetime.now, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
