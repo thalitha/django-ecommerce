4347536eaa2a70f124ea8fa6d55ee9cbeedc7b35
thalitha <campos.talita@ymail.com>
2019-12-14 12:56:16 +0000

    creation of the first product

--- ecommerceapp/admin.py
+++ ecommerceapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Product, Order, Item
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Item)
