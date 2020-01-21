fd1f1a373c5f438e11878364be4c0575993b8493
thalitha <campos.talita@ymail.com>
2020-01-21 00:17:20 +0000

    Changed the path of image

--- ecommerceapp/models.py
+++ ecommerceapp/models.py
@@ -9,7 +9,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_date = models.DateTimeField(default=datetime.now, blank=True)
     category = models.CharField(max_length=200)
-    image = models.ImageField(upload_to="img/%d/%m/%Y/", blank=True, null=True)
+    image = models.ImageField(upload_to="img/", blank=True, null=True)
     published = models.BooleanField(default=False)
     def __str__(self):
         return self.name
