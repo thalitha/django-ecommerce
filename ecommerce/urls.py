5eac920389261bb8f6f4277fc404ff3b5f892dfd
thalitha <47127508+thalitha@users.noreply.github.com>
2020-01-29 09:08:47 +0000

    Update urls.py

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path('', include('ecommerceapp.urls')),
     path('payments/', include('payments.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
